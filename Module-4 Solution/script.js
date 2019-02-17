76199510ebf731d5e7c0902c86a79050faf2cdc5
ShubhamPariha <46998250+ShubhamPariha@users.noreply.github.com>
2019-02-17 23:51:12 +0530

    Update script.js

--- Module-4 Solution/script.js
+++ Module-4 Solution/script.js
@@ -14,15 +14,13 @@ Hello Paula
 Hello Laura
 Good Bye Jim
 */
-
-(function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
+            console.log("Good Bye "+ names[i]);
         } else {
-            helloSpeaker.speak(names[i]);
+            console.log("Hello "+ names[i]);
         }
     }
 })();
